b029c8acabfb09e84d7b7f3ef1f1df4cb6d7b383
Fabricio <fabricio.seb1@gmail.com>
2022-04-29 18:22:42 -0300

    finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import './App.css';
 import Formulario from './components/form';
 import Lista from './components/list';
@@ -8,6 +8,7 @@ import { ITarefa } from './types/tarefa';
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [onlyComplete, setOnlyComplete] = useState(false);
 
   function deleteTask(id: string) {
     return setTarefas(tarefas.filter((tarefa) => tarefa.id !== id))
@@ -23,13 +24,35 @@ function App() {
     }))
   }
 
+  function verFinalizadas() {
+    setOnlyComplete(true);
+  }
+  function verPendentes() {
+    setOnlyComplete(false);
+  }
+
   return (
     <div className="App">
       <h1>
         Lista de tarefas
       </h1>
+      <div className='linha'>
+        <h5>
+          total: {tarefas.length}
+        </h5>
+        <h5>
+          pendentes: {tarefas.filter((tarefa) => tarefa.done===false).length}
+        </h5>
+        <h5>
+          concluídas: {tarefas.filter((tarefa) => tarefa.done===true).length}
+        </h5>
+      </div>
         <Formulario setTarefas={setTarefas}></Formulario>
-        <Lista tarefas={tarefas} deleteTask={deleteTask} endTask={endTask}></Lista>
+        <div className='linha' >
+          <button onClick={() => verPendentes()}>Ver Pendentes</button>
+          <button onClick={() => verFinalizadas()}>Ver Concluídas</button>
+        </div>
+        <Lista tarefas={tarefas} deleteTask={deleteTask} endTask={endTask} onlyComplete={onlyComplete}></Lista>
     </div>
   );
 }
--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,25 +1,46 @@
+import { useEffect } from "react";
 import { ITarefa } from "../../types/tarefa";
 // import Item from "./item";
 
 interface Props {
     tarefas: ITarefa[],
     deleteTask: (id: string) => void,
-    endTask: (id: string) => void
+    endTask: (id: string) => void,
+    onlyComplete: boolean
 }
 
-function Lista({ tarefas, deleteTask, endTask }: Props) {
+function Lista({ tarefas, deleteTask, endTask, onlyComplete }: Props) {
+    useEffect( () => {
+        var axios = require('axios');
+
+    var config = {
+      method: 'get',
+      url: 'https://trainees-2022-todo-api-week-3.herokuapp.com/todos/',
+      headers: { }
+    };
+    
+    axios(config)
+    .then(function (response: { data: any; }) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error: any) {
+      console.log(error);
+    });
+    },[])
+
     return(
         <aside>
-                {tarefas.map((item, index) => {
+                {
+                tarefas.filter((tarefa) => tarefa.done===onlyComplete ).map((item, index) => {
                     return(
                     // <Item
                     //     {...item}
                     // />
                     
-                        <div className={`tarefa ${item.done ? 'finalizado' : ''}`} key={index}>
+                        <div className="tarefa" key={index}>
                             <h3>{item.tarefa}</h3>
                             <button onClick={() => deleteTask(item.id)}>deletar</button>
-                            <button onClick={() => endTask(item.id)}>terminar tarefa</button>
+                            {!item.done && <button onClick={() => endTask(item.id)}>terminar tarefa</button>}
                         </div>
                     
                 )})}
